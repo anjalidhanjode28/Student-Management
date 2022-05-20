856dbafe4295a3e58a9a2e6bb29cb068604f0074
anjalidhanjode28 <99669975+anjalidhanjode28@users.noreply.github.com>
2022-05-20 16:25:06 +0530

    file added

---
+++ student_management/script/index.js
@@ -0,0 +1,30 @@
+document.getElementById("submit").addEventListener("click", add)
+
+var array = JSON.parse(localStorage.getItem("students")) || [];
+
+let count =  document.getElementById('counter');
+count.innerHTML = array.length;
+
+function add(event){
+    event.preventDefault()
+    var name = document.getElementById("name").value;
+    var course = document.getElementById("course").value;
+    var unit = document.getElementById("unit").value;
+    var image = document.getElementById("image").value;
+    var batch = document.getElementById("batch").value;
+
+    var obj = {
+        name: name,
+        course: course,
+        unit: unit,
+        image: image,
+        batch: batch,
+    }
+    array.push(obj)
+    localStorage.setItem("students", JSON.stringify(array))
+
+}
+
+document.getElementById("dashboard").addEventListener("click", function(){
+    window.location.href = "./dashboard.html"
+})
